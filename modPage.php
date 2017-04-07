69401e72e7670bdca9947d98c55f02dbd3495224
dfcurrie <dfcurrie@ucalgary.ca>
2017-04-07 00:06:12 -0600

    mod page

---
+++ modPage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Mod Page</title>
+	</head>
+	
+	<style>
+		h1 {
+			width: 500px;
+			margin: 50px auto;
+		}
+		form {
+			display: inline;
+		}
+		.button {
+			width: 250px;
+			padding: 6px 15px;
+			border: 2px solid #3498DB;
+			background-color: #3498DB;
+			color: white;
+		}
+		.button:hover {
+			background-color: #fafafa;
+			color: #207cca;
+		}
+
+	</style>
+	
+	<body>
+		<?php include 'header.php' ?>
+		
+		<font color="#3498DB"><center><h1>Welcome to the Super Secret Moderator Page</h1></center></font>
+
+		
+	</body>
+</html>
