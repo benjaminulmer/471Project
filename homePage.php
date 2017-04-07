6e26effb0aa2c459cfa0d27a04618c0c7e34f7fb
Retinizer <alex4d@live.ca>
2017-04-06 23:19:55 -0600

    Removed home button from home page

--- homePage.php
+++ homePage.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 
 <html>
@@ -20,14 +23,59 @@
 			background-color: #3498DB;
 			color: white;
 		}
+		.button2 {
+			position: absolute;
+			top: 0;
+			right: 81px;
+			padding: 6px 15px;
+			color: #3498DB;
+		}
+		.button3 {
+			position: absolute;
+			top: 0;
+			right: 0;
+			padding: 6px 15px;
+			border: 2px solid #3498DB;
+			background-color: #3498DB;
+			color: #fafafa;
+		}
 		.button:hover {
+			background-color: #fafafa;
+			color: #207cca;
+		}
+		.button2:hover {
+			background-color: #fafafa;
+			color: #207cca;
+		}
+		.button3:hover {
 			background-color: #fafafa;
 			color: #207cca;
 		}
 	</style>
 	
 	<body>
-		<?php include 'header.php' ?>
+		<form action="homePage.php">
+		<input type="submit" class="button2" value="Home">
+		</form>
+		<?php
+		if (isset($_SESSION['login_user'])){
+			?>
+			
+			<form action="logout.php">
+			<input type="submit" class="button3" value="Log Out: <?php echo $_SESSION['login_user']; ?>">
+			</form>
+			<?php
+		} else {
+		?>
+			<form action="loginPage.php">
+			<input type="submit" class="button2" value="Log In">
+			</form>
+			<form action="signUpPage.php">
+			<input type="submit" class="button3" value="Sign Up">
+			</form>
+			<?php
+		}
+		?>
 
 		<font color="#3498DB"><center><h1>Ultimate Movie Database of Ultimate Destiny</h1></center></font>
 		<center>
